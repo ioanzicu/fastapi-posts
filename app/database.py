50b213209d6e78b60ed39161bebdabf28f40681c
Zicu Ioan <ioan.zicu@nokia.com>
2021-12-24 16:41:52 +0100

    environment variables

--- app/database.py
+++ app/database.py
@@ -4,9 +4,10 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -23,15 +24,15 @@ def get_db():
         db.close()
 
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi',
-                                user='postgres', password='postgres',
-                                cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('Database connection was succesfull!!!')
-        break
-    except Exception as exc:
-        print('Connection to database failed!')
-        print(exc)
-        time.sleep(2)
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi',
+#                                 user='postgres', password='postgres',
+#                                 cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print('Database connection was succesfull!!!')
+#         break
+#     except Exception as exc:
+#         print('Connection to database failed!')
+#         print(exc)
+#         time.sleep(2)
--- app/oauth2.py
+++ app/oauth2.py
@@ -4,6 +4,7 @@ from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -13,9 +14,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = 'cee8c2e65d91024f33cc35bffe090ad52b1ad10a023534f1dadd813fb16299be'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
