ed873b6c687b0a878d5bc5923fd6bda7b7dab8e1
Zicu Ioan <ioan.zicu@nokia.com>
2021-12-26 23:44:55 +0100

    create & drop tabels after each test

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,16 +7,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 from app.database import get_db, Base
+import pytest
 
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
-# create tables in db based on models
-Base.metadata.create_all(bind=engine) 
 
 def override_get_db():
     db = TestingSessionLocal()
@@ -25,20 +25,40 @@ def override_get_db():
     finally:
         db.close()
 
+
 app.dependency_overrides[get_db] = override_get_db
 
-client = TestClient(app)
 
-def test_root():
+# runs before each test
+@pytest.fixture
+def client():
+    Base.metadata.drop_all(bind=engine)  # drop tables
+    # set up - run out code before we run our test
+    Base.metadata.create_all(bind=engine)  # create tables
+    yield TestClient(app)
+    # cleanup - run our test after our test finished
+
+    # OR
+    # Base.metadata.create_all(bind=engine) # create tables
+    # yield TestClient(app)
+    # Base.metadata.drop_all(bind=engine)  # drop tables
+
+    # OR - alembic
+    # command.upgrade('head')
+    # yield TestClient(app)
+    # command.downgrade('base')
+
+
+def test_root(client):
     response = client.get('/')
-    assert response.json().get('message') == 'Hi, I see that a good man is visiting us. I am glad that you opened this app. Wish you a good day! ;)'
+    assert response.json().get(
+        'message') == 'Hi, I see that a good man is visiting us. I am glad that you opened this app. Wish you a good day! ;)'
     assert response.status_code == status.HTTP_200_OK
 
 
-
-def test_create_user():
-    response = client.post('/users/', json={'email': 'thebest-alpaca@mail.com', 'password': 'alpaca1234'})
+def test_create_user(client):
+    response = client.post(
+        '/users/', json={'email': 'thebest-alpaca@mail.com', 'password': 'alpaca1234'})
     new_user = schemas.UserOut(**response.json())
     assert new_user.email == 'thebest-alpaca@mail.com'
     assert response.status_code == status.HTTP_201_CREATED
-
