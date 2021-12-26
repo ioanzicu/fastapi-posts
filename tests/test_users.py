ad0aaed12834b7b536239cb441b54869e09cccd4
Zicu Ioan <ioan.zicu@nokia.com>
2021-12-26 23:54:53 +0100

    move test db setup to separate file

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,45 +1,6 @@
 from fastapi import status
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from sqlalchemy import create_engine, engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.config import settings
-from app.database import get_db, Base
-import pytest
-
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
-
-@pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)  # drop tables
-    Base.metadata.create_all(bind=engine)  # create tables
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-# runs before each test
-@pytest.fixture
-def client(session):  # will run after session fixture
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
+from .database import client, session
 
 
 def test_root(client):
